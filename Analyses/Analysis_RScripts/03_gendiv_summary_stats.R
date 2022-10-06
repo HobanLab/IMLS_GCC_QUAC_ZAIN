290537f13e4badedc8224550d839062090c4956d
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2022-10-06 14:32:48 -0500

    updating the gendiv summary stats script to incorporate parallelization to speed up analysis

--- Analyses/Analysis_RScripts/03_gendiv_summary_stats.R
+++ Analyses/Analysis_RScripts/03_gendiv_summary_stats.R
@@ -1,9 +1,9 @@
-##Here we calculate summary statistics 
-#First, we tested linkage disequilibrium, null alleles, and Hardy Weinberg 
+###In this script, we calculated genetic diversity summary statistics 
+##First, we tested linkage disequilibrium, null alleles, and Hardy Weinberg 
 #equilibrium. Next, a data frame including expected heterozygosity, 
 #allelic richness, number of alleles, mean longitude and latitude 
 #for wild populations, and individual numbers. 
-#This table is included in full in the supplemental text of this manuscript.
+#The result tables are included in full in the supplemental text of this manuscript.
 #We used files in this script that are referred to as "clean," which means
 #clones and individuals with too much missing data have been removed. 
 
@@ -17,6 +17,8 @@ library(hierfstat)
 library(PopGenReport)
 library(pegas)
 library(diveRsity)
+library(parallel)
+library(doParallel)
 
 #########################
 #   Load Data Files     #
@@ -36,54 +38,68 @@ scenario_list <- c("QUAC_wK", "QUAC_woK", "ZAIN_og", "ZAIN_rebinned")
 ############################################################
 #  Null Alleles, HWE Deviation, Linkage Disequilibrium     #
 ############################################################
-#loop to assess scores for null alleles, HWE deviations, and linkage disequilibrium
+#loop to save genind objects
+sp_genind_temp <- list()
+
+#loop to load in genind objects and run HWE devitation test
 for(sp in 1:length(sp_genind_list)){
   
   #load genepop files as genind objects 
-  sp_genind_temp <- read.genepop(paste0("Adegenet_Files/", sp_genind_list[[sp]]), ncode = 3)
+  sp_genind_temp[[sp]] <- read.genepop(paste0("Adegenet_Files/", sp_genind_list[[sp]]), ncode = 3)
   
   #load data frames 
   sp_df_temp <- read.csv(paste0("Data_Frames/", sp_df_list[[sp]]))
   
   #organize genind object
-  rownames(sp_genind_temp@tab) <- sp_df_temp[,1]
-  levels(sp_genind_temp) <- unique(sp_df_temp[,2])
+  rownames(sp_genind_temp[[sp]]@tab) <- sp_df_temp[,1]
+  levels(sp_genind_temp[[sp]]) <- unique(sp_df_temp[,2])
   
-  ##run prelim genetic diversity assessments  
-  #calculate % of null alleles/locus
-  sp_null_all <- null.all(sp_genind_temp)
-  
-  #create data frame
-  sp_null_all_df <- signif(data.frame(sp_null_all$null.allele.freq$summary2),3)
-  
-  #write out data frame
-  write.csv(sp_null_all_df, paste0("../Analyses/Results/Sum_Stats/", scenario_list[[sp]] , "_null_all_df.csv"))
-  
+  ##HWE devitations 
   #run HWE deviations by pop 
-  sp_HWE_pop <- seppop(sp_genind_temp) %>% lapply(hw.test, B = 1000)
+  sp_HWE_pop <- seppop(sp_genind_temp[[sp]]) %>% lapply(hw.test, B = 1000)
   
-  #create df by pop 
+  #create df by pop for HWE devitations
   sp_HWE_pop_df <- sapply(sp_HWE_pop, "[", i = TRUE, j = 3)
   
+  ##HWE deviation data frame prep
   #name columns with populations
   colnames(sp_HWE_pop_df) <- unique(sp_df_temp[,2])
-  
   #round to the 3rd digit
   sp_HWE_allpop_df <- signif(sp_HWE_pop_df, 3)
+  #write out HWE deviation data files
+  write.csv(sp_HWE_allpop_df, paste0("../Analyses/Results/Sum_Stats/", scenario_list[[sp]], 
+                                     "_HWE_dev_pop.csv"))
   
-  #write out 
-  write.csv(sp_HWE_allpop_df, paste0("../Analyses/Results/Sum_Stats/", scenario_list[[sp]], "_HWE_dev_pop.csv"))
   
-  #calculate linkage disequilibrium 
-  sp_ld <- pair.ia(sp_genind_temp, sample = 1000)
-  
-  #convert to a data frame
-  sp_ld_df <- data.frame(round(sp_ld,digits = 2))
-  
-  #write out 
-  write.csv(sp_ld_df, paste0("../Analyses/Results/Sum_Stats/", scenario_list[[sp]], "_LD.csv"))
-  
+   
 }
+
+#calculate % of null alleles/locus
+#use parallelization because this step takes quite a while without
+#Calculate the number of cores
+cores <- detectCores() - 1
+#Initiate cluster
+cl <- makeCluster(cores)
+
+#run null allele calculations over all genind objects
+sp_null_all <- parLapply(cl, sp_genind_temp, null.all)
+
+#create null allele frequency summary data frame
+sp_null_all_df <- signif(data.frame(sp_null_all[[sp]]$null.allele.freq$summary2),3)
+
+#write out to CSV
+write.csv(sp_null_all_df, paste0("../Analyses/Results/Sum_Stats/", scenario_list[[sp]] , "_null_all_df.csv"))
+
+#calculate linkage disequilibrium 
+#sp_ld <- pair.ia(sp_genind_temp, sample = 1000)
+
+#convert to a data frame
+#sp_ld_df <- data.frame(round(sp_ld,digits = 2))
+
+#write out 
+#write.csv(sp_ld_df, paste0("../Analyses/Results/Sum_Stats/", scenario_list[[sp]], "_LD.csv"))
+
+
 
 ###########################################
 #          Genetic Stats by Pop           #
