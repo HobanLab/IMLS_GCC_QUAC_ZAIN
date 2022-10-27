a853aac59cd91ad7da38101bdcf3e3130190d8f5
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2022-10-27 13:38:18 -0500

    updating the maternal accession code with the new function

--- Analyses/Analysis_RScripts/09_maternal_accessions.R
+++ Analyses/Analysis_RScripts/09_maternal_accessions.R
@@ -7,7 +7,6 @@
 #########################
 
 library(tidyverse)
-library(Demerelate)
 
 #########################
 #   Load Data Files     #
@@ -16,56 +15,26 @@ library(Demerelate)
 setwd("../../Data_Files")
 
 #load in accession records 
-sp_accession_list <- list.files(path = "Data_Frames", 
-                                pattern = "accession")
+sp_accession_list <- list.files(path = "Data_Frames", pattern = "accession")
 
-sp_df_list <- c("QUAC_woK_allpop_clean_df.csv","ZAIN_rebinned_allpop_clean_df.csv")
-
+#create list for species
 sp_list <- c("QUAC", "ZAIN")
+
+#load in function to calculate maternal accessions for plants 
+source("../Analyses/Functions/maternal_accession.R")
 
 ###################################
 #     Maternal plant records      #
 ###################################
-#loop over scenarios to calculate the relatedness levels based on half siblings
-for(m in 1:length(sp_df_list)){
-  #loop over maternal accession information 
-  for(a in 1:length(sp_accession_list)){
-
-  #load in data frame for relatedness analysis  
-  sp_temp_df <- read.csv(paste0("Data_Frames/",sp_df_list[[m]]))
+#loop over maternal accession information 
+for(a in 1:length(sp_accession_list)){
   
   #load in data frame with maternal accession information 
-  sp_accessions <- read.csv(paste0("Data_Frames/",sp_accession_list[[a]]))
+  sp_accessions <- read.csv(paste0("Data_Frames/",sp_accession_list[[1]]))
 
-  #run relatedness code to generate relatedness data frame 
-  relate_df <- halfsib_relate_df_loiselle(sp_temp_df)
-  
-  #limit accession data by cleaned data 
-  sp_temp_garden_df <- sp_temp_df[sp_temp_df$Garden_Wild == "Garden",]
-  
-  if(m == 1 & a == 1){
+  #run maternal accession code 
+  mat_df <- mat_accession(sp_accessions)
     
-    sp_temp_accession_df <- sp_accessions[sp_accessions[,1] %in% sp_temp_garden_df[,1],]
-    
-    #run maternal accession code 
-    mat_df <- accession_count(sp_temp_accession_df)
-    
-  rel_mat_df <- cbind(relate_df, mat_df)
-  
-  write.csv(rel_mat_df, "../Analyses/Results/Garden_Wild_Comparison/QUAC_rel_mat_df.csv")
-  }
-  if(m == 2 & a == 2){
-
-    sp_temp_accession_df <- sp_accessions[sp_accessions[,1] %in% sp_temp_garden_df[,1],]
-    
-    #run maternal accession code 
-    mat_df <- accession_count(sp_temp_accession_df)
-    rel_mat_df <- cbind(relate_df, mat_df)
-    
-    write.csv(rel_mat_df, "../Analyses/Results/Garden_Wild_Comparison/ZAIN_rel_mat_df.csv")
-    
-  }
-
-  }
-
+  #write out maternal accession data frame
+  write.csv(mat_df, paste0("../Analyses/Results/Garden_Wild_Comparison/", sp_list[[a]], "_mat_acc_df.csv"))
 }
